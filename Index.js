d658f85efd79b74c9857443c13d24931f9381411
olawande samuel <samueldacoal@gmail.com>
2021-04-19 13:20:52 +0100

    update

--- Index.js
+++ Index.js
@@ -1,26 +1,10 @@
-const http = require("http");
 const fs = require("fs");
-const path = require("path");
-
 const fetch = require("node-fetch");
-
-// if (!fs.existsSync("result")) {
-//   fs.mkdir("result", (err) => {
-//     if (err) return err;
-
-//     fs.writeFile("./result/posts.json", newData, (err) => {
-//       if (err) return err;
-//       console.log("directory created");
-//     });
-//   });
-// }
 
 fetch("http://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((data) => {
     const newData = JSON.stringify(data);
-    
-
     if (!fs.existsSync("result")) {
       fs.mkdir("result", (err) => {
         if (err) return err;
